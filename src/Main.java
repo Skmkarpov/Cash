1367be6316ce86fa0c06e185347d9b74215a5e78
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2023-02-25 09:38:13 +0300

    Задание 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 100; //счёт клиента
-        int c = 2000; //сумма пополнения
-        int b; // бонус
-        if (c > 1000) { b = c/100;
-        } else { b = 0;
+        int CustomerAccount = 100; //счёт клиента
+        int RepAmount = 500; //сумма пополнения
+        int bonus; // бонус
+        if (RepAmount > 1000) { bonus = RepAmount/100;
+        } else { bonus = 0;
         }
-        int z = x+c+b;
-        int v = z-(x+c);
-        System.out.println("Итоговый счёт = " + z);
-        System.out.println("Бонусные мили = " + v);
+        int FinalScore = CustomerAccount+RepAmount+bonus;
+        int BonusMiles = FinalScore-(CustomerAccount+RepAmount);
+        System.out.println("Итоговый счёт = " + FinalScore);
+        System.out.println("Бонусные мили = " + BonusMiles);
     }
 }
